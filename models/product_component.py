a65c74e01c0c5de84172b11f7857733758976190
Reven24 <reven24@gmail.com>
2025-11-29 03:24:51 +0200

    добавлен мультиязычный интерфейс ru/ua

--- models/product_component.py
+++ models/product_component.py
@@ -1,34 +1,31 @@
-# 
 # --- МОДУЛЬ: КОМПОНЕНТЫ
 # --- \dino24_addons\dino_erp_stock\models\product_component.py 
 #
-from odoo import fields, models
+from odoo import fields, models, _, api
 
 class DinoProductComponent(models.Model):
     _name = 'dino.product.component'
     _description = 'Dino Product Component'
-    # Наследуем миксины для фото, чаттера и избранного
     _inherit = ['image.mixin', 'mail.thread'] 
-    
-    active = fields.Boolean('Active', default=True)
-    name = fields.Char(string='Component Name', required=True, tracking=True)
-    code = fields.Char(string='Internal Reference', copy=False, default='New', tracking=True)
 
-    # Поля, добавленные по запросу:
-    qty_available = fields.Float(string='On Hand', default=0.0) # В наличии
+    active = fields.Boolean('Active', default=True)
     
-    currency_id = fields.Many2one('res.currency', string='Currency', 
+    # Используем _() чтобы Odoo точно знал, что это метка для перевода
+    name = fields.Char(string=_('Component Name'), required=True, tracking=True, translate=True)
+    code = fields.Char(string=_('Internal Reference'), copy=False, default='New', tracking=True)
+
+    qty_available = fields.Float(string=_('On Hand'), default=0.0) 
+    
+    currency_id = fields.Many2one('res.currency', string=_('Currency'), 
                                   default=lambda self: self.env.company.currency_id)
-    cost = fields.Monetary(string='Cost', currency_field='currency_id', default=0.0) # Стоимость
     
-    # Функциональность избранного
-    is_favorite = fields.Boolean(string='Favorite')
+    cost = fields.Monetary(string=_('Cost'), currency_field='currency_id', default=0.0)
+    is_favorite = fields.Boolean(string=_('Favorite'))
 
     _sql_constraints = [
         ('code_unique', 'unique (code)', 'The internal reference must be unique.'),
     ]
     
-    # Метод для переключения избранного (используется кнопкой в форме)
     def toggle_is_favorite(self):
         for rec in self:
             rec.is_favorite = not rec.is_favorite
